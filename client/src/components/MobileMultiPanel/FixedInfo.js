4f6feb6193cd0465ba7306da979e9ff6dfc3b3c3
Craig Nockels <cnockels@gmail.com>
2020-07-17 20:34:42 -0400

    fixedinfo bar basic

--- client/src/components/MobileMultiPanel/FixedInfo.js
+++ client/src/components/MobileMultiPanel/FixedInfo.js
@@ -1,14 +1,48 @@
 import React from 'react'; 
 import styled from 'styled-components'; 
+import { useSelector } from 'react-redux';
+
+import { distanceTo } from '../../utils';
+
+import paper from '../../assets/paper.jpg';
 
 // visible at top of panel at all times?
 // Velocity, Bearing, Elevation, NearestCity(dist)
 
 const FixedInfo = () => { 
+  //info for velocity and bearing
+  const { elevation, direction } = useSelector( state => state.user.profile);
+  const { windSum, windBearing } = useSelector( state => state.conditions.current);
+  //info for nearest city and it's distance
+  const { nearestCity } = useSelector(state => state.conditions);
+  const { location } = useSelector((state) => state.user.profile);
+
 
   return (
     <StyledDiv> 
-      <div> FixedInfo </div>
+      <Section>
+        <Descriptor>Velocity: </Descriptor>
+        <Values>
+          {parseInt(windSum * elevation)}
+        </Values>
+      </Section>
+      <Section>
+        <Descriptor>Bearing: </Descriptor>
+        <Values>
+          {(windBearing + direction)>360? 
+          (windBearing + direction -360) 
+          : (windBearing + direction)}°
+        </Values>
+      </Section>
+      <Section> 
+        <Descriptor>Nearest City: </Descriptor>
+        <Values>
+          {nearestCity.tags.name}
+          {' [ '} 
+          {(distanceTo([nearestCity.lat, nearestCity.lon], location)/1000).toFixed()} 
+          {' km]'}
+        </Values>
+      </Section>
     </StyledDiv> 
   ) 
 }; 
@@ -18,5 +52,24 @@ export default FixedInfo;
 
 
 const StyledDiv = styled.div`
-
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: .5rem;
+  background-image: url(${paper});
+  border-top: 3px solid gray;
+  border-bottom: 3px solid gray;
+`;
+const Section = styled.div`
+  display: inline-block;
+  span {
+    font-family: 'Rye', cursive;
+  }
+`;
+const Descriptor = styled.span`
+  color: black;
+`;
+const Values = styled.span`
+  color: #36454f;
 `;
---
+++ client/src/utils.js
@@ -0,0 +1,9 @@
+//DISTANCE BETWEEN TWO GEOGRAPHICAL POINTS//
+export const distanceTo = (pointA, pointB) => {
+  const φ1 = pointA[0] * Math.PI/180;
+  const φ2 = pointB[0] * Math.PI/180;
+  const Δλ = (pointB[1]-pointA[1]) * Math.PI/180;
+  const R = 6371e3;
+  const distance = Math.acos( Math.sin(φ1)*Math.sin(φ2) + Math.cos(φ1)*Math.cos(φ2) * Math.cos(Δλ) ) * R;
+  return distance;
+};
